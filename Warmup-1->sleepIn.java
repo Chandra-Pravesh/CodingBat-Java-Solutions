251852f56a71bcc348613f8f973bfa244379bf4f
Chandra Pravesh <129057746+Chandra-Pravesh@users.noreply.github.com>
2026-05-11 12:01:27 +0530

    Create Warmup-1->sleepIn.java

---
+++ Warmup-1->sleepIn.java
@@ -0,0 +1,14 @@
+public boolean sleepIn(boolean weekday, boolean vacation) {
+  if (vacation==true){
+    return true;
+  }
+  else{
+    if(weekday==false){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  
+}
