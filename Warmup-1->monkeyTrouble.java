63c69788f96befe65f4b5a38b1c5710ef4837b3c
Chandra Pravesh <129057746+Chandra-Pravesh@users.noreply.github.com>
2026-05-11 12:08:27 +0530

    monkeyTrouble.java

---
+++ Warmup-1->monkeyTrouble.java
@@ -0,0 +1,18 @@
+public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+  if (aSmile == true){
+    if(bSmile==true){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  else{
+     if(bSmile==false){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
